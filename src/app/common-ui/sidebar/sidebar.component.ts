fbb2a6d494fb842478a6cf38408e564390b8f9b0
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-01-25 19:49:55 +0300

    feat: sidebar menu

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink, TranslatePipe, SvgIconComponent, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  menu: { name: string; link: string; icon: string }[] = [
+    {
+      name: 'MY_PAGE',
+      link: 'profile',
+      icon: 'home',
+    },
+    {
+      name: 'CHATS',
+      link: 'chats',
+      icon: 'chats',
+    },
+    {
+      name: 'SEARCH',
+      link: '/',
+      icon: 'search',
+    },
+  ];
+}
