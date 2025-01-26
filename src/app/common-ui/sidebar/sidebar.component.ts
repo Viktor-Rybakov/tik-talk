b32f2dbe0e406edecc5dc43eca0607ef2a4d21ab
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-01-26 21:05:58 +0300

    feat: sidebar footer

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -2,13 +2,13 @@ import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
-import { map, Observable } from 'rxjs';
+import { firstValueFrom, map, Observable } from 'rxjs';
 
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 import { SubscriberCardComponent } from '../subscriber-card/subscriber-card.component';
 import { type Profile } from '../../data/interfaces/profile.iterface';
 import { ProfileRestService } from '../../data/services/profile-rest.service';
-
+import { AvatarUrlPipe } from '../../helpers/pipes/avatar-url.pipe';
 
 @Component({
   selector: 'app-sidebar',
@@ -19,12 +19,15 @@ import { ProfileRestService } from '../../data/services/profile-rest.service';
     RouterLinkActive,
     SubscriberCardComponent,
     AsyncPipe,
+    AvatarUrlPipe,
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
   private profileRestService = inject(ProfileRestService);
+
+  me = this.profileRestService.me;
 
   menu: { name: string; link: string; icon: string }[] = [
     {
@@ -47,4 +50,8 @@ export class SidebarComponent {
   subscribers$: Observable<Profile[]> = this.profileRestService
     .getSubscribers()
     .pipe(map((response) => response.items.slice(0, 3)));
+
+  constructor() {
+    firstValueFrom(this.profileRestService.getMe());
+  }
 }
--- src/app/data/services/profile-rest.service.ts
+++ src/app/data/services/profile-rest.service.ts
@@ -1,6 +1,6 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 import { type Profile } from '../interfaces/profile.iterface';
 import { type Pageable } from '../interfaces/pageable.interface';
@@ -13,12 +13,18 @@ const ApiPrefix: string = 'https://icherniakov.ru/yt-course/';
 export class ProfileRestService {
   private _http = inject(HttpClient);
 
+  me = signal<Profile | null>(null);
+
   getProfiles(): Observable<Profile[]> {
     return this._http.get<Profile[]>(`${ApiPrefix}account/test_accounts`);
   }
 
   getMe(): Observable<Profile> {
-    return this._http.get<Profile>(`${ApiPrefix}account/me`);
+    return this._http.get<Profile>(`${ApiPrefix}account/me`).pipe(
+      tap((response) => {
+        this.me.set(response);
+      })
+    );
   }
 
   getSubscribers(): Observable<Pageable<Profile>> {
