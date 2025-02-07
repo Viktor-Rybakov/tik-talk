f81f88f94a7003fe486601dd211da19fb54be195
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-02-07 23:10:06 +0300

    feat: profile header

---
+++ src/app/common-ui/profile-header/profile-header.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { AvatarUrlPipe } from '../../helpers/pipes/avatar-url.pipe';
+import { Profile } from '../../data/interfaces/profile.iterface';
+
+@Component({
+  selector: 'app-profile-header',
+  imports: [AvatarUrlPipe],
+  templateUrl: './profile-header.component.html',
+  styleUrl: './profile-header.component.scss',
+})
+export class ProfileHeaderComponent {
+  @Input({ required: true }) profile!: Profile;
+}
