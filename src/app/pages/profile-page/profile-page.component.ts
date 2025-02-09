04389d26c32f6ba5aa0e2c94b19ef2f11150ecd7
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-02-09 21:25:48 +0300

    feat: profile-info

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,17 +1,28 @@
 import { Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { toObservable } from '@angular/core/rxjs-interop';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 import { TranslatePipe } from '@ngx-translate/core';
 
 import { ProfileHeaderComponent } from '../../common-ui/profile-header/profile-header.component';
 import { ProfileRestService } from '../../data/services/profile-rest.service';
 import { SvgIconComponent } from '../../common-ui/svg-icon/svg-icon.component';
+import { AvatarUrlPipe } from '../../helpers/pipes/avatar-url.pipe';
+import { PostFeedComponent } from './post-feed/post-feed.component';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [ProfileHeaderComponent, AsyncPipe, RouterLink, SvgIconComponent, TranslatePipe],
+  imports: [
+    ProfileHeaderComponent,
+    AsyncPipe,
+    RouterLink,
+    SvgIconComponent,
+    TranslatePipe,
+    AvatarUrlPipe,
+    JsonPipe,
+    PostFeedComponent,
+  ],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
 })
@@ -19,9 +30,15 @@ export class ProfilePageComponent {
   private profileService = inject(ProfileRestService);
   private route = inject(ActivatedRoute);
 
+  isMe: boolean = false;
+
   me$ = toObservable(this.profileService.me);
+  subscribers$ = this.profileService.getSubscribersShortList(6);
 
   profile$ = this.route.params.pipe(
+    tap(({ id }) => {
+      this.isMe = id === 'me';
+    }),
     switchMap(({ id }) => {
       if (id === 'me') {
         return this.me$;
