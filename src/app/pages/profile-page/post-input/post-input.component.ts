9a8f27c58c2977054afbf125050c8f7399ec5616
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-02-18 11:33:37 +0300

    feat: post input added

--- src/app/pages/profile-page/post-input/post-input.component.ts
+++ src/app/pages/profile-page/post-input/post-input.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, HostBinding, Input } from '@angular/core';
+
+import { AvatarComponent } from '../../../common-ui/avatar/avatar.component';
+import { SvgIconComponent } from '../../../common-ui/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-post-input',
-  imports: [],
+  imports: [AvatarComponent, SvgIconComponent],
   templateUrl: './post-input.component.html',
-  styleUrl: './post-input.component.scss'
+  styleUrl: './post-input.component.scss',
 })
 export class PostInputComponent {
-
+  @HostBinding('class.comment')
+  @Input({ alias: 'comment', transform: booleanAttribute })
+  isComment: boolean = false;
 }
