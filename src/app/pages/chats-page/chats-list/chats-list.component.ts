cc5d291bcab04b83b67f385516cd602d76d5e280
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-03-03 22:30:29 +0300

    feat: chat list updated

--- src/app/pages/chats-page/chats-list/chats-list.component.ts
+++ src/app/pages/chats-page/chats-list/chats-list.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { map, startWith, switchMap } from 'rxjs';
+
+import { ChatsService } from '../../../data/services/chats.service';
 import { ChatButtonComponent } from './chat-button/chat-button.component';
+import { SvgIconComponent } from '../../../common-ui/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-chats-list',
-  imports: [ChatButtonComponent],
+  imports: [
+    ChatButtonComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    SvgIconComponent,
+    AsyncPipe,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './chats-list.component.html',
   styleUrl: './chats-list.component.scss',
 })
-export class ChatsListComponent {}
+export class ChatsListComponent {
+  #chatsService = inject(ChatsService);
+
+  searchControl = new FormControl<string>('', { nonNullable: true });
+
+  chats$ = this.#chatsService.getMyChats().pipe(
+    switchMap((chats) =>
+      this.searchControl.valueChanges.pipe(
+        startWith(''),
+        map((searchValue) => {
+          return chats.filter((chat) => {
+            return `${chat.userFrom.firstName} ${chat.userFrom.lastName}`
+              .toLowerCase()
+              .includes(searchValue.toLowerCase().trim());
+          });
+        })
+      )
+    )
+  );
+}
