b843fc75bd7df1afffe922fe7047ff08792f3d5a
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-03-03 22:29:03 +0300

    feat: chat button updated

--- src/app/pages/chats-page/chats-list/chat-button/chat-button.component.ts
+++ src/app/pages/chats-page/chats-list/chat-button/chat-button.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+import { type LastMessage } from '../../../../data/interfaces/chats.interface';
+import { AvatarComponent } from '../../../../common-ui/avatar/avatar.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-chat-button',
-  imports: [],
+  selector: 'button[chat]',
+  imports: [AvatarComponent, DatePipe],
   templateUrl: './chat-button.component.html',
-  styleUrl: './chat-button.component.scss'
+  styleUrl: './chat-button.component.scss',
 })
 export class ChatButtonComponent {
-
+  chat = input.required<LastMessage>();
 }
