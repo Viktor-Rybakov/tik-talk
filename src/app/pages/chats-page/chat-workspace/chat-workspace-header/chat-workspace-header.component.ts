035afbb7e58bbf8ba9591552fa371ec92b9b9ae9
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-03-04 10:49:44 +0300

    feat: chat messages added

--- src/app/pages/chats-page/chat-workspace/chat-workspace-header/chat-workspace-header.component.ts
+++ src/app/pages/chats-page/chat-workspace/chat-workspace-header/chat-workspace-header.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+import { type Profile } from '../../../../data/interfaces/profile.interface';
+import { AvatarComponent } from '../../../../common-ui/avatar/avatar.component';
 
 @Component({
   selector: 'app-chat-workspace-header',
-  imports: [],
+  imports: [AvatarComponent],
   templateUrl: './chat-workspace-header.component.html',
-  styleUrl: './chat-workspace-header.component.scss'
+  styleUrl: './chat-workspace-header.component.scss',
 })
 export class ChatWorkspaceHeaderComponent {
-
+  profile = input.required<Profile>();
 }
