369b84492bf07615920381872f62ca01f741ba9a
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-03-04 22:41:11 +0300

    feat: chat messages grouped by day

--- src/app/data/services/chats.service.ts
+++ src/app/data/services/chats.service.ts
@@ -14,7 +14,7 @@ export class ChatsService {
   #http = inject(HttpClient);
   me = inject(ProfileService).me;
 
-  activeChatMessages = signal<Message[]>([]);
+  activeChatMessagesMap = signal<Map<string, Message[]>>(new Map());
 
   createChat(userId: number) {
     return this.#http.post<Chat>(`${ApiPrefix}chat/${userId}`, {});
@@ -35,7 +35,19 @@ export class ChatsService {
           };
         });
 
-        this.activeChatMessages.set(patchedMessages);
+        const messagesMap = new Map<string, Message[]>();
+
+        patchedMessages.forEach((message: Message) => {
+          const messageCreateDate = new Date(message.createdAt).toISOString().slice(0, 10);
+
+          if (!messagesMap.has(messageCreateDate)) {
+            messagesMap.set(messageCreateDate, []);
+          }
+
+          messagesMap.get(messageCreateDate)!.push(message);
+        });
+
+        this.activeChatMessagesMap.set(messagesMap);
 
         return {
           ...chat,
