952cf8aee9f8db18639deaf06b01a7d9879a15b8
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-03-07 21:17:27 +0300

    sandbox: page fixed

--- src/app/sandbox/sandbox-page/sandbox-page.component.ts
+++ src/app/sandbox/sandbox-page/sandbox-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TemplateFormComponent } from '../template-form/template-form.component';
+import { ReactiveFormComponent } from '../reactive-form/reactive-form.component';
 
 @Component({
   selector: 'app-sandbox-page',
-  imports: [TemplateFormComponent],
+  imports: [TemplateFormComponent, ReactiveFormComponent],
   templateUrl: './sandbox-page.component.html',
   styleUrl: './sandbox-page.component.scss',
 })
