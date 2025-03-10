d19b87a53462058d47212bc0fd70dc72e37439bf
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-03-10 23:28:24 +0300

    sandbox: reactive forms updated

---
+++ src/app/sandbox/legal-recipient-name.validator.ts
@@ -0,0 +1,46 @@
+import { AbstractControl, AsyncValidator, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { inject, Injectable } from '@angular/core';
+import { MockService } from './mock.service';
+import { delay, map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LegalRecipientNameValidator implements AsyncValidator {
+  #mockService = inject(MockService);
+
+  // через стрелочную функцию сделано для того, чтобы при сохранить this (либо привязываем через bind)
+  // validate = (control: AbstractControl): Observable<ValidationErrors | null> => {
+  //   return this.#mockService.getLegalRecipientName().pipe(
+  //     delay(1000),
+  //     map((names: string[]) => {
+  //       const namesNormalized = names.map((name) => name.toLowerCase());
+  //
+  //       return namesNormalized.includes(control.value.toLowerCase())
+  //         ? null
+  //         : {
+  //           legalRecipientNameValid: {
+  //             message: `Название организации должно быть одно из списка: ${names.join(', ')}`,
+  //           },
+  //         };
+  //     })
+  //   );
+  // }
+
+  validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    return this.#mockService.getLegalRecipientName().pipe(
+      delay(1000),
+      map((names: string[]) => {
+        const namesNormalized = names.map((name) => name.toLowerCase());
+
+        return namesNormalized.includes(control.value.toLowerCase())
+          ? null
+          : {
+              legalRecipientNameValid: {
+                message: `Название организации должно быть одно из списка: ${names.join(', ')}`,
+              },
+            };
+      })
+    );
+  }
+}
---
+++ src/app/sandbox/mock.service.ts
@@ -0,0 +1,55 @@
+import { Observable, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Address } from './form.interface';
+
+export interface AdditionOptions {
+  id: string;
+  name: string;
+  value: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MockService {
+  getAddresses(): Observable<Address[]> {
+    return of([
+      {
+        city: 'Самара',
+        street: 'Московское шоссе',
+        building: 34,
+        apartment: 326,
+      },
+      {
+        city: 'Санкт-Петербург',
+        street: 'Пл. Восстания',
+        building: 1,
+        apartment: 22,
+      }
+    ]);
+  }
+
+  getLegalRecipientName(): Observable<string[]> {
+    return of(['Организация', 'Ростех', 'Росмех']);
+  }
+
+  getFeatures(): Observable<AdditionOptions[]> {
+    return of([
+      {
+        id: 'lift',
+        name: 'Подъем на этаж',
+        value: true,
+      },
+      {
+        id: 'strong-package',
+        name: 'Усиленная упаковка',
+        value: true,
+      },
+      {
+        id: 'fast',
+        name: 'Ускоренная доставка',
+        value: false,
+      },
+    ]);
+  }
+}
