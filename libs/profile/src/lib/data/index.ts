60ef45adfee827af4729e2dcbbe541a0cb12bb3d
Viktor Rybakov <viktor.rybakov@gmail.com>
2025-03-15 23:54:49 +0300

    feature(store): profile filters initial value added

--- libs/profile/src/lib/data/index.ts
+++ libs/profile/src/lib/data/index.ts
@@ -1,4 +1,6 @@
+import { ProfilesFilter, ProfilesFilterForm } from './interfaces/profilesFilter';
 import { ProfileService } from './services/profile.service';
 export * from './store';
 
 export { ProfileService };
+export type { ProfilesFilter, ProfilesFilterForm };
--- libs/profile/src/lib/data/store/actions.ts
+++ libs/profile/src/lib/data/store/actions.ts
@@ -1,11 +1,12 @@
 import { createActionGroup, props } from '@ngrx/store';
 
 import { Profile } from '@tt/interfaces/profile';
+import { ProfilesFilter } from '../interfaces/profilesFilter';
 
 export const profileActions = createActionGroup({
   source: 'Profile',
   events: {
-    filterEvent: props<{ filters: Record<string, any> }>(),
+    filterEvent: props<{ filters: Partial<ProfilesFilter> }>(),
     profilesLoaded: props<{ profiles: Profile[] }>(),
   },
 });
